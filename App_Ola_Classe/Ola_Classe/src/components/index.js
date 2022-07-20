b8b1a48386302ae8d0d26e14039da70e62fadb9f
Alexxmfs <alexxmfs@hotmail.com>
2022-07-20 11:16:56 -0300

    sdadsadasd

--- App_Ola_Classe/Ola_Classe/src/components/index.js
+++
@@ -1,5 +0,0 @@
-import { CircleButton, ButtonRegister, CustomInput, CustomInputDate } from "./Button";
-
-export {
-    CircleButton, ButtonRegister, CustomInput, CustomInputDate, ButtonCreateAccount
-}
